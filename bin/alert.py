b01ec0c53cf911b028555dfe9453751893222015
h-akira <79441679+h-akira@users.noreply.github.com>
2023-11-25 22:30:32 +0900

    add new options

--- bin/alert.py
+++ bin/alert.py
@@ -8,8 +8,10 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../lib")
 from  google_api_operator.authentication import get_service
 from google_api_operator.gmail import send_mail
+from google.auth.exceptions import RefreshError
 import json
 import datetime
+import traceback
 from pytz import timezone
 import pandas_datareader.data as web
 import yfinance as yf
@@ -29,12 +31,15 @@ def parse_args():
     help="interval"
   )
   parser.add_argument("-p", "--plenty", metavar="seconds", type=int, default=5, help="現在時刻を何秒前とするか")
-  parser.add_argument("-o", "--output", metavar="output-file", default="output", help="output file")
-  parser.add_argument("--log", metavar="log-file", default=os.path.join(os.path.dirname(__file__), "../secret/log.txt"), help="output file")
+  parser.add_argument("--log", metavar="log-file", default=os.path.join(os.path.dirname(__file__), "../secret/log.txt"), help="log file")
+  parser.add_argument("--error-log", metavar="error-log-file", default=os.path.join(os.path.dirname(__file__), "../secret/error.txt"), help="log file")
+  parser.add_argument("--state", metavar="state-file", default=os.path.join(os.path.dirname(__file__),"../secret/state.txt"), help="state file")
   parser.add_argument("-t", "--token", metavar="Path", default=os.path.join(os.path.dirname(__file__),"../secret/token.json"), help="token.json（存在しない場合は生成される）")
   parser.add_argument("-c", "--credentials", metavar="Path", default=os.path.join(os.path.dirname(__file__),"../secret/credentials.json"), help="credentials.json（client_secret_hogehoge.json）")
-  parser.add_argument("-r", "--re-authenticate", action="store_true", help="token.jsonを削除して再認証する")
+  parser.add_argument("-f", "--force-execution", action="store_true", help="state fileを削除して強制実行する")
+  parser.add_argument("-r", "--re-authenticate", action="store_true", help="token fileを削除して再認証する")
   parser.add_argument("-s", "--service", action="store_true", help="通知メールを送信しない場合でもtokenが有効かどうか確認のためserviceを取得する")
+  parser.add_argument("--no-stdout", action="store_true", help="no stdout")
   parser.add_argument("file", metavar="json-file", help="json file")
   options = parser.parse_args()
   if not os.path.isfile(options.file): 
@@ -50,6 +55,9 @@ def parse_args():
   if os.path.isfile(options.token) and options.re_authenticate:
     print(f"Delete `{options.token}` to re-authenticate.")
     os.remove(options.token)
+  if options.force_execution and os.path.isfile(options.state):
+    print(f"Delete `{options.state}` to force execution.")
+    os.remove(options.state)
   return options
 
 def cross_checker(df, long=20, short=9, inclination=False):
@@ -92,8 +100,54 @@ def cross_checker(df, long=20, short=9, inclination=False):
   else:
     return None
 
-def main():
-  options = parse_args()
+def preprocessing(opitons):
+  if options.state:
+    if os.path.isfile(options.state):
+      with open(options.state,mode="r") as f:
+        state = f.read().strip()
+        if state == "Running":
+          print("It is not possible to execute more than one at the same time.")
+          sys.exit()
+        elif state == "Failed":
+          print("It failed the last time it was run. please check.")
+          print(f"After that, Please delete `{opitons.state}` or execute with `--force-execution`")
+          sys.exit()
+        elif state == "Error":
+          print("An error occurred during the previous execution. please check.")
+          print(f"After that, Please delete `{opitons.state}` or execute with `--force-execution`")
+          sys.exit()
+        elif state == "Expired":
+          print(f"`options.token` is invalid. Please delete and reacquire.")
+          print(f"After that, Please delete `{options.state}` or execute with `--force-execution`")
+          sys.exit()
+    with open(options.state,mode="w") as f:
+      print("Running",file=f)
+
+def postprocessiong(options, success=True, error=False, token=False):
+  if options.state:
+    if error:
+      with open(options.state,mode="w") as f:
+        if token:
+          print("Expired",file=f)
+          print(f"`{options.token}` is invalid. Please delete and reacquire.")
+          print(f"After that, Please delete `{options.state}` or execute with `--force-execution`")
+        else:
+          print("Error",file=f)
+          if os.path.isfile(options.error_log):
+            f =  open(options.error_log, mode='a')
+          else:
+            f =  open(options.error_log, mode='w')
+          print(f"===== {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ====", file=f)
+          traceback.print_exc(file=f)
+          f.close()
+    else:
+      with open(options.state,mode="w") as f:
+        if success:
+          print("Successed",file=f)
+        else:
+          print("Failed",file=f)
+
+def main(options):
   config = json.load(open(options.file))
   if options.log:
     with open(options.log, mode="r") as f:
@@ -162,6 +216,19 @@ def main():
       send_mail(**args)
       print("Sent mail. Message is below.")
       print(message)
+    return True  # Trueを返すと`Successed`，Falseを返すと`Failed`になる．
 
 if __name__ == '__main__':
-  main()
+  options = parse_args()
+  if options.no_stdout:
+    sys.stdout = open(os.devnull, 'w')
+  try:
+    preprocessing(options)
+    success = main(options)
+    postprocessiong(options, success=success)
+  except RefreshError:
+    postprocessiong(options, error=True, token=True)
+  except SystemExit:
+    pass
+  except:
+    postprocessiong(options, error=True)
--- secret/README.md
+++ secret/README.md
@@ -1,2 +1,25 @@
 # secret
-`credentials.json`, `token.json`, `log.txt`をこのディレクトリに置く．
+`credentials.json`, `token.json`, `log.txt`, `error.txt`, `state.txt`を
+このディレクトリに置く．
+## credentials.json
+認証情報が記述されたファイル．
+## token.json
+ログインすることで得られるトークンファイル．
+## log.txt
+取得できたレートの最終日時を通貨ペアとともに記録したファイル．
+これとの重複を調べることで
+週末など市場が閉じているときに
+繰り返し通知メールを送信してしまうことを
+防止する．
+## error.txt
+エラーが発生した時にその内容を記録する．
+ただし，トークンの有効期限が切れたことによる
+`RefreshError`は記録しない．
+## state.txt
+`alert.py`が複数同時に実行されるのを防止するために状態を記録する．
+実行中である場合は`Runnning`，
+前回実行時に`RefreshError`が発生した場合は`Expired`，
+他のエラーが発生した場合は`Error`，
+失敗した場合は`Failed`，
+成功した場合は`Successed`
+と記録される．
